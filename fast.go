e80eddf83e7239ef4f34076fe62d5cb4a3d6b4be
Madi <madi.baidolda@gmail.com>
2021-05-24 12:28:02 +0600

    1

---
+++ fast.go
@@ -0,0 +1,103 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
+	"regexp"
+	"strings"
+)
+
+// вам надо написать более быструю оптимальную этой функции
+func FastSearch(out io.Writer) {
+	file, err := os.Open(filePath)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	fileContents, err := ioutil.ReadAll(file)
+	if err != nil {
+		panic(err)
+	}
+
+	r := regexp.MustCompile("@")
+	seenBrowsers := []string{}
+	uniqueBrowsers := 0
+	foundUsers := ""
+
+	lines := strings.Split(string(fileContents), "\n")
+
+	users := make([]map[string]interface{}, 0)
+	for _, line := range lines {
+		user := make(map[string]interface{})
+		err := json.Unmarshal([]byte(line), &user)
+		if err != nil {
+			panic(err)
+		}
+		users = append(users, user)
+	}
+
+	for i, user := range users {
+
+		isAndroid := false
+		isMSIE := false
+
+		browsers, ok := user["browsers"].([]interface{})
+		if !ok {
+			continue
+		}
+
+		for _, browserRaw := range browsers {
+			browser, ok := browserRaw.(string)
+			if !ok {
+				continue
+			}
+			if ok, err := regexp.MatchString("Android", browser); ok && err == nil {
+				isAndroid = true
+				notSeenBefore := true
+				for _, item := range seenBrowsers {
+					if item == browser {
+						notSeenBefore = false
+					}
+				}
+				if notSeenBefore {
+					seenBrowsers = append(seenBrowsers, browser)
+					uniqueBrowsers++
+				}
+			}
+		}
+
+		for _, browserRaw := range browsers {
+			browser, ok := browserRaw.(string)
+			if !ok {
+				continue
+			}
+			if ok, err := regexp.MatchString("MSIE", browser); ok && err == nil {
+				isMSIE = true
+				notSeenBefore := true
+				for _, item := range seenBrowsers {
+					if item == browser {
+						notSeenBefore = false
+					}
+				}
+				if notSeenBefore {
+					seenBrowsers = append(seenBrowsers, browser)
+					uniqueBrowsers++
+				}
+			}
+		}
+
+		if !(isAndroid && isMSIE) {
+			continue
+		}
+
+		email := r.ReplaceAllString(user["email"].(string), " [at] ")
+		foundUsers += fmt.Sprintf("[%d] %s <%s>\n", i, user["name"], email)
+	}
+
+	fmt.Fprintln(out, "found users:\n"+foundUsers)
+	fmt.Fprintln(out, "Total unique browsers", len(seenBrowsers))
+}
